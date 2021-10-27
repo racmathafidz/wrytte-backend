f1e65d2fc70f08a0d55e101846d0bc2a7cf3e986
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2021-10-27 13:35:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  
   credentials: true,
 }));
