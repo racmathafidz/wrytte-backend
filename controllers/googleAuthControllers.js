51048e3c2dffa59a572de42b91094f2de94a283c
racmathafidz <rac.hafidz@gmail.com>
2021-11-04 13:06:03 +0700

    feat(authentication): add google authentication

--- controllers/googleAuthControllers.js
+++ controllers/googleAuthControllers.js
@@ -1,22 +1,54 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
-const passport = require('passport');
+const { OAuth2Client } = require('google-auth-library');
 
-const google_auth_get = () => {
-  // the 'authenticate' process located in passport's configs strategy
-  passport.authenticate('google', {
-    // getting data scopes
-    scope: ['profile', 'email'],
+const User = require('../models/User');
+const keys = require('../config/keys');
+
+const client = new OAuth2Client(keys.google.clientID);
+
+const google_auth_post = async (req, res) => {
+  const { token } = req.body;
+
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: keys.google.clientID,
   });
+
+  const { name, email, picture } = ticket.getPayload();
+
+  User.findOne({ email })
+    .then(async (response) => {
+      if (response) {
+        req.session.userId = response.id;
+        res.status(201).send(response);
+      } else {
+        const userName = email.split('@');
+        const user = await User.create({
+          email,
+          userName: userName[0],
+          fullName: name,
+          imageProfile: picture,
+          google: true,
+        });
+
+        req.session.userId = user.id;
+        res.status(201).send(user);
+      }
+    });
 };
 
-const google_redirect_get = () => {
-  passport.authenticate('google', (req, res) => {
-    res.status(200).send(req.user);
-  });
+const google_signout_get = (req, res) => {
+  req.session.destroy();
+  req.logout();
+};
+
+const google_user_get = (req, res) => {
+  res.status(200).send(req.user);
 };
 
 module.exports = {
-  google_auth_get,
-  google_redirect_get,
+  google_auth_post,
+  google_signout_get,
+  google_user_get,
 };
