74062ab007e78d3f607a2d92ba6f673cbcf56d69
racmathafidz <rac.hafidz@gmail.com>
2021-11-04 13:14:26 +0700

    feat(router): add signout router

--- routes/localAuthRoutes.js
+++ routes/localAuthRoutes.js
@@ -4,7 +4,8 @@ const localAuthControllers = require('../controllers/localAuthControllers');
 
 const router = Router();
 
-router.post('/signin', localAuthControllers.signin_post);
-router.post('/signup', localAuthControllers.signup_post);
+router.post('/signin', localAuthControllers.local_signin_post);
+router.post('/signup', localAuthControllers.local_signup_post);
+router.get('/signout', localAuthControllers.local_signout_get);
 
 module.exports = router;
