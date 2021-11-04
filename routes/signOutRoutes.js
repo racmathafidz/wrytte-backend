5f908c863e9d6daffd2a8a66eecd400238e83398
racmathafidz <rac.hafidz@gmail.com>
2021-11-04 13:07:32 +0700

    delete

--- routes/signOutRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const signOutControllers = require('../controllers/signOutControllers');
-
-const router = Router();
-
-router.get('/', signOutControllers.signout_get);
-
-module.exports = router;
