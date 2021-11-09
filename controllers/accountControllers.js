bf9f84083f89f736c20527cb3a4459d67ffd8dcf
racmathafidz <rac.hafidz@gmail.com>
2021-11-09 13:20:59 +0700

    feat(account route and controller): add user's photo upload and account update feature

--- controllers/accountControllers.js
+++ controllers/accountControllers.js
@@ -1,6 +1,34 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
+const DatauriParser = require('datauri/parser');
+const path = require('path');
+
 const User = require('../models/User');
 const Article = require('../models/Article');
+const { uploader } = require('../config/cloudinary');
+
+// Parsing buffer to string
+const parser = new DatauriParser();
+
+const photo_upload_post = (req, res) => {
+  if (req.file) {
+    // Parsing from buffer to string
+    const file = parser.format(
+      path.extname(req.file.originalname).toString(),
+      req.file.buffer,
+    ).content;
+
+    // Uploading
+    uploader.upload(file)
+      .then((result) => {
+        console.log(result);
+        res.status(200).send({ imageProfile: result.secure_url });
+      })
+      .catch((err) => {
+        res.status(400).send({ msg: err });
+      });
+  }
+};
 
 const account_profile_get = async (req, res) => {
   try {
@@ -20,6 +48,24 @@ const account_profile_get = async (req, res) => {
   }
 };
 
+const account_profile_update = (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(`Update ${id}`);
+    console.log(req.body);
+    // Option object { new: true } is for sending  updated data response
+    User.findByIdAndUpdate(id, req.body, { new: true })
+      .then((response) => {
+        console.log(response);
+        res.status(200).send(response);
+      });
+  } catch (error) {
+    res.status(500).send({ message: 'Internal Server Error', error });
+  }
+};
+
 module.exports = {
+  photo_upload_post,
   account_profile_get,
+  account_profile_update,
 };
