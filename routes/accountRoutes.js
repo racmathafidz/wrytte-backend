90e86899e5a41aeb2627aecd243e57fd62751f5b
racmathafidz <rac.hafidz@gmail.com>
2021-11-10 04:15:44 +0700

    refactor(picture upload feature): removing picture upload routes and controllers

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
 
 const accountControllers = require('../controllers/accountControllers');
-const { cloudinaryConfig, multerUploads } = require('../config/cloudinary');
 
 const router = Router();
 
 // Apllying cloudinary config middleware
-router.use('/*', cloudinaryConfig);
-router.post('/image-upload', multerUploads, accountControllers.photo_upload_post);
 router.get('/:userName', accountControllers.account_profile_get);
 router.put('/:id', accountControllers.account_profile_update);
 
