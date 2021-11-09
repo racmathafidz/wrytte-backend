e1bb1e0fd1011344cf71f95114d771c655ae0e45
racmathafidz <rac.hafidz@gmail.com>
2021-11-09 13:17:23 +0700

    feat(cloudinary): add cloudinary utils for uploading image to cloud

---
+++ config/cloudinary.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-len */
+const { config, uploader } = require('cloudinary').v2;
+const multer = require('multer');
+
+// CLoudinary config
+const cloudinaryConfig = (req, res, next) => {
+  config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+  });
+  next();
+};
+
+// Multer config
+const storage = multer.memoryStorage();
+const multerUploads = multer({ storage }).single('file');
+// single's argument (here is 'file') must be the same with the input type on client's html tag
+
+module.exports = {
+  cloudinaryConfig,
+  uploader,
+  multerUploads,
+};
